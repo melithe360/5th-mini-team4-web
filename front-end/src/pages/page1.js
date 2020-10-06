4f3822267348fae30d061bd6df4a54005d56069b
이희진 <melithe360@gmail.com>
2020-10-06 16:25:50 +0900

    page1 test

---
+++ front-end/src/pages/page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './page1.css';
+
+function page1(){
+    return (
+        <div>
+            <div className="header"><h1 class="title">제목</h1></div>
+        </div>
+    )
+}
+
+export default page1;
