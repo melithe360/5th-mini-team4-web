e8b2622f65382485dd360eeaedb348eb00b136f2
이희진 <melithe360@gmail.com>
2020-10-13 15:53:55 +0900

    page component push: heejin

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Assessment from './components/Assessment/Assessment';
+import "./App.css";
+import Page1 from '../src/pages/page1'
 
 function App() {
   return (
     <div className="App">
       5th-mini-project
       <Assessment></Assessment>
+      <Page1 />
     </div>
   );
 }
--- front-end/src/pages/page1.js
+++ front-end/src/pages/page1.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './page1.css';
 
-function page1(){
+function Page1(){
     return (
-        <div>
-            <div className="header"><h1 class="title">제목</h1></div>
-        </div>
+                 <div>
+                     <div class="header">
+                     <h1 class="title">하정인 기획&개발&디자인 능력 설문</h1>
+
+                     <li id="exversions">      이전 버전
+                        <ul id="submenu">
+                            <li><a href="#" id="index1">2020.10.1</a></li>
+                            <li><a href="#" id="index2">2020.10.2</a></li>
+                            <li><a href="#" id="index3">2020.10.3</a></li>
+                            <li><a href="#" id="index4">2020.10.4</a></li>
+                        </ul>
+                    </li>
+                    <button class="send" onClick={onclick}>보내기</button>
+                    </div>
+                    <div class="body">내용</div>
+                </div>
     )
 }
 
-export default page1;
+export default Page1;
